00a4565748b733780e97c2e5dbfed931e9c5a806
Oleh <olehtomas@gmail.com>
2015-05-21 08:12:15 +0300

    Update calc.cpp

--- 1-course/Calculator/calc.cpp
+++ 1-course/Calculator/calc.cpp
@@ -1,149 +1,187 @@
-/********************************************************************************/
-/*  File name: main.cpp                                                         */
-/*                                                                              */
-/*  Lab Calculator, Variant A                                                   */
-/*  URL = http://cyb.univ.kiev.ua/~tkarnaukh/Programming/Semestr2/Lab%205.pdf   */
-/*                                                                              */
-/*  Author: Lamzin Oleh                                                         */
-/*          olehtomas@gmail.com                                                 */
-/*                                                                              */
-/*  Time: 06:19:46 05/21/15                                                     */
-/*                                                                              */
-/********************************************************************************/
-//g++ -std=c++11 main.cpp calc.cpp 
-
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <string.h>
-
+/**********************************************************/
+/*  File name: calc.cpp                                   */
+/*                                                        */
+/*  Author: Lamzin Oleh                                   */
+/*          olehtomas@gmail.com                           */
+/*                                                        */
+/*  Time: 06:19:46 05/21/15                               */
+/*                                                        */
+/**********************************************************/
+#include <math.h>
+#include <vector>
 #include "calc.h"
 
+#include <iostream>
 using namespace std;
 
-void user();
-void file(char *name);
-void testing();
-void print_help();
-void print_info();
+#define what_is(x) cerr << #x << " = " << (x) << endl;
 
-double my_abs(double x){
-    return ((x) > 0.0) ? (x) : -(x);  
-} 
+node::node(char ch) : isop(true), operation(ch), number(0.0f){}
+node::node(double num) : isop(false), operation('$'), number(num){}
 
-int main(int argc, char **argv){
-    print_info();
+bool torpn::isDelim(char ch){
+    return ch == ' ';
+}
 
-    if (argc < 2){
-        cerr << "I don't what to do...";
-        print_help();
-        return 0;
-    }
+bool torpn::isOperation(char ch){
+    return ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '#';
+}
 
-    if (strcmp(argv[1],"-h") == 0){
-        print_help();
-        return 0;
-    }
+bool torpn::isNum(char ch){
+    return ch == '.' || (ch >= '0' && ch <= '9');
+}
 
-    if (strcmp(argv[1],"-u") == 0){
-        user();
-        return 0;
-    }
+bool torpn::isUnary(char ch){
+    return ch == '+' || ch == '-';
+}
 
-    if (strcmp(argv[1],"-f") == 0){
-        if (argc < 3){
-            cerr << "Invalid command, enter file name in command line.";
-            return 0;
+int torpn::priority(char op){
+    return
+        op == '#' ? 3 :
+        op == '+' || op == '-' ? 1 :
+        op == '*' || op == '/' ? 2 :
+        -1;
+}
+
+vector<node> torpn::str_to_rpn(string s){
+    vector<node> rpn;
+    vector<char> op;
+    double sign = 1.0f;
+
+    for(int i = 0; i < s.length(); i++){
+        if (isDelim(s[i])) continue;
+        if (s[i] == '('){
+            op.push_back('(');
+            continue;
         }
-        file(argv[2]);
-        return 0;
+        if (s[i] == ')'){
+            while(op.size()){
+                char top = op.back(); op.pop_back();
+                if (top == '(') break;
+                rpn.push_back(node(top));
+            }
+            continue;
+        }
+        if (isUnary(s[i]) && s[i - 1] == '('){
+            continue;
+        }
+        if (isOperation(s[i])){
+            while(op.size() && priority(op.back()) >= priority(s[i])){
+                rpn.push_back(node(op.back()));
+                op.pop_back();
+            }
+            op.push_back(s[i]);
+            continue;
+        }
+        string operand;
+        if (i - 2 >= 0  && s[i - 2] == '(' 
+            && 
+            (s[i - 1] == '-' || s[i - 1] == '+')) operand += '-';
+        while(i < s.length() && isNum(s[i])) operand += s[i++];
+        --i;
+
+        std::size_t error;
+        double numb = std::stod(operand, &error);
+        rpn.push_back(node(numb));
     }
 
-    if (strcmp(argv[1],"-t") == 0){
-        testing();
-        return 0;
+    return rpn;
+}
+
+string calc::prepare(string s){
+    s = "(" + s + ")";
+    std::size_t found = s.find("exp");
+    while(found != std::string::npos){
+        s.replace(found, 3, "#");                
+        found = s.find("exp");
+    }
+    found = s.find(" ");
+    while(found != std::string::npos){
+        s.replace(found, 1, "");                
+        found = s.find(" ");
+    }
+    found = s.find("-#");
+    while(found != std::string::npos){
+        s.replace(found, 2, "-1*#");                
+        found = s.find("-#");
+    }
+    found = s.find("(-(");
+    while(found != std::string::npos){
+        s.replace(found, 3, "(0-(");                
+        found = s.find("(-(");
     }
 
-    cerr << "Invalid command.\n";
-    return 0;
+    return s;
 }
 
-void user(){
-    calc calculator;
-    cout << "Enter expression: ";
-    cout << "Result: " << calculator.calculate(cin) << endl;
+bool calc::inAlphabet(char ch){
+    return  ch == '+' || ch == '-' || 
+            ch == '*' || ch == '/' || 
+            ch == '#' || ch == '.' || 
+            ch == '(' || ch == ')' || ch == ' ' || 
+            (ch >= '0' && ch <= '9');
 }
 
-void file(char *name){
-    fstream file(name, ios::in);
-    if (!file.is_open()){
-        cerr << "File not open!\n";
-        return;
+bool calc::inSpecialSymbols(char ch){ // This symbol can*t to be near!
+    return  ch == '+' || ch == '-' || 
+            ch == '*' || ch == '/' || 
+            ch == '.';
+}
+
+bool calc::isExpression(string s){
+    int nested = 0;
+    for (int i = 0; i<s.length(); i++){
+        if (!inAlphabet(s[i])) return false;
+        if (i && inSpecialSymbols(s[i]) && inSpecialSymbols(s[i - 1])) return false;
+        if (i && s[i] == '#' && (s[i - 1] == '.' || s[i - 1] == '#')) return false;
+        if (s[i] == '(') nested++;
+        if (s[i] == ')') nested--;
+        if (nested < 0) return false;
+    }
+    if (nested != 0) return false;
+    return true;
+}
+
+double calc::calculate(istream &out){
+    string s;
+    getline(out, s);
+    return calculate(s);
+}
+
+double calc::calculate(string str){
+    string prep = prepare(str);
+    if(!isExpression(prep)){
+        cout << "(ERROR: Invalid expression) ";
+        return 0.0f;
     }
 
-    calc calculator;
-    cout << "Result: " << calculator.calculate(file) << endl;
-    file.close();
-}
+    vector<node> rpn = torpn::str_to_rpn(prep); 
+    vector<double> value_stack;
 
-void testing(){
-    calc calculator;
-    vector<pair<string, double> > tests;
-    int AC_count = 0;
-
-    tests.push_back(make_pair("-5.5-2*(12+56/12.32*12.2+exp(2.2))", -158.459117907));
-    tests.push_back(make_pair("123/12.123*exp(-2/228+3/exp(1))", 30.3242060857));
-    tests.push_back(make_pair("-exp(4)/exp(3)", -2.71828182459));
-    tests.push_back(make_pair("1+2+3+4+5+6+7+8+9", 45));
-    tests.push_back(make_pair("( -( -( -( -( -( -(-1) ) ) ) ) ) )", -1));
-    tests.push_back(make_pair("exp( exp(1/2 * (3 - 2) ) )", 5.2003257647899));
-    tests.push_back(make_pair("13+46*56+456/6-456/4978*12.123+15", 2678.889496183));
-    tests.push_back(make_pair("-(435+21*(2/exp(3)))+16/exp(1/exp(3))", -421.8681449850));
-    tests.push_back(make_pair("1*2*3*4*5*6*7*8*9*10", 3628800));
-    tests.push_back(make_pair("2 + 2", 4));
-    //https://www.wolframalpha.com/ - calculate it!
-
-    cout << "Test result:\n";
-    for (int i = 0; i<tests.size(); i++){
-        double res = calculator.calculate(tests[i].first);
-        cout << "    " << tests[i].first << endl;
-        cout << "    Expected result: " << tests[i].second << endl;
-        cout << "    Result: " << res << endl;
-        cout << "    " << (my_abs(res - tests[i].second) < 0.000001 ? "OK" : "WA") << endl;       
-        AC_count += my_abs(res - tests[i].second) < 0.000001;
+    for(int i = 0; i < rpn.size(); i++){
+        if (rpn[i].isop){
+            double result;
+            if (rpn[i].operation == '#'){
+                double operand = value_stack.back(); value_stack.pop_back();
+                result = exp(operand);
+            }
+            else{
+                double r = value_stack.back(); value_stack.pop_back();
+                double l = value_stack.back(); value_stack.pop_back();
+                switch(rpn[i].operation){
+                    case '+':
+                        result = l + r; break;
+                    case '-':
+                        result = l - r; break;
+                    case '*':
+                        result = l * r; break;
+                    case '/':
+                        result = l / r; break;
+                }
+            }
+            value_stack.push_back(result);
+        }
+        else value_stack.push_back(rpn[i].number);
     }
-    cout << "AC: " << AC_count << " / " << tests.size() << endl;
-}
-
-void print_help(){
-    cout << "\nHelp:\n"
-         << "    -h: help\n" 
-         << "    -u: user's expression\n"
-         << "    -f file_name: expression in the file\n"
-         << "    -t: auto testing\n";
-}
-
-void print_info(){
-    cout << "Lamzin Oleh, K12 - Lab Calculator, variant A.\n\n"
-         << "Task: realize expression calculator with constants.\n"
-         << "    depending on the command-line arguments the program should:\n"
-         << "        -u : to ask the user the expression and calculate its\n"
-         << "        -f file_name: to calculate recorded in a file expression, user enters the file name\n"
-         << "        -t : to execute automatic testing and display the result\n\n"
-
-         << "BNF:\n"
-         << "terminals:  '+', '-', '*', '/', '(', ')', 'x', 'exp' // x - double\n"
-         << "nonterminals: <expression>, <term>, <factor>\n"
-         << "products:\n"
-         << "    <expression> → <expression> + <term>\n"
-         << "    <expression> → <expression> - <term>\n"
-         << "    <expression> → <term>\n"
-         << "    <term> → <term> * <factor>\n"
-         << "    <term> → <term> / <factor>\n"
-         << "    <term> → exp(<factor>)\n"
-         << "    <term> → <factor>\n"
-         << "    <factor> → (<expression>)\n"
-         << "    <factor> → x,\n"
-         << "the initial nonterminal <expression>.\n\n\n";
-         //URL ="https://en.wikipedia.org/wiki/Context-free_grammar"
+    return value_stack[0];
 }
