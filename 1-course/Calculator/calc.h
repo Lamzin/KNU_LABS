3d1b846c6ce3b0a1dd151a92b1dfea5e5b686f46
Oleh <olehtomas@gmail.com>
2015-06-05 04:17:43 +0300

    Update calc.h

--- 1-course/Calculator/calc.h
+++ 1-course/Calculator/calc.h
@@ -26,14 +26,14 @@ struct node{
 
 class torpn{
 private:
-    static bool isDelim(char ch);
-    static bool isOperation(char ch);
-    static bool isNum(char ch);
-    static bool isUnary(char ch);
-    static int priority(char op);
+    bool isDelim(char ch);
+    bool isOperation(char ch);
+    bool isNum(char ch);
+    bool isUnary(char ch);
+    int priority(char op);
 
 public:
-    static vector<node> str_to_rpn(string s);
+    vector<node> str_to_rpn(string s, bool &err);
 };
 
 class calc{
@@ -41,9 +41,9 @@ private:
     bool isExpression(string s);
     bool inAlphabet(char ch); 
     bool inSpecialSymbols(char ch);
-    string prepare(string s);
+    string prepare(string s, bool &err);
 
 public:
-    double calculate(string str);
-    double calculate(istream &out);
+    double calculate(string str, bool &err);
+    double calculate(istream &out, bool &err);
 };
