7b75ecd8aae51505d5c6d5757e3aeeeea6ee102c
Oleh <Lamzin@users.noreply.github.com>
2016-12-30 14:47:20 +0200

    use bcmath library

--- 3-course/sharapov-lab/partition.php
+++ 3-course/sharapov-lab/partition.php
@@ -3,53 +3,46 @@
 
 <?php
 class Foo {
-
     function main($index, $n) {
         $combinations = $this->getAllSumCombinations($n);
-
         $enough = false;
-        $skipped = 0;
+        $skipped = "0";
         $combination = $combinations[0];
         for ($i = 0; $i < count($combinations); $i++) {
             $c = $this->getPartitionCountInBlock($combinations[$i]);
-            if ($skipped + $c >= $index) {
+            if (bccomp(bcadd($skipped, $c), $index) >= 0) {
                 $combination = $combinations[$i];
                 $enough = true;
                 break;
             }
-            $skipped += $c;
+            $skipped = bcadd($skipped, $c);
         }
         if (!$enough) {
             return "fatal: index too big";
         }
-
         $used = array();
-        $used_count = 0;
+        $used_count = "0";
         for ($i = 0; $i < $n; $i++) {
-            array_push($used, -1);
+            array_push($used, "-1");
         }
-
-        $index -= $skipped;
-        $skipped = gmp_init(0);
-        $set_index = 0;
+        $index = bcsub($index, $skipped);
+        $skipped = "0";
+        $set_index = "0";
         while ($used_count < $n) {
             for ($j = 0; $j < count($combination); $j++) {
                 if ($j > 0 && $combination[$j - 1] == $combination[$j]) {
                     continue;
                 }
-
                 $tmp_combination = $combination;
                 unset($tmp_combination[$j]);
                 $tmp_combination = array_values($tmp_combination);
-
                 $x = $this->getPartitionCountInBlock($tmp_combination);
-                $c = $this->getCombinations($combination[$j] - 1, $n - $used_count - 1);
-
-                if (gmp_cmp($skipped + $c * $x, $index) < 0) {
-                    $skipped = gmp_add($skipped, $c * $x);
+                $c = $this->getCombinations(bcsub($combination[$j], "1"), bcsub(bcsub($n, $used_count), "1"));
+                if (bccomp(bcadd($skipped, bcmul($c, $x)), $index) < 0) {
+                    $skipped = bcadd($skipped, bcmul($c, $x));
                 } else {
-                    $combination_number = intval(floor(($index - $skipped - 1) / $x) + 1);
-                    $set_combination = $this->getCombinationByIndex($combination_number, $combination[$j] - 1, $n - $used_count - 1);
+                    $combination_number = bcadd(bcdiv(bcsub(bcsub($index, $skipped), "1"), $x), "1");
+                    $set_combination = $this->getCombinationByIndex($combination_number, bcsub($combination[$j], "1"), bcsub(bcsub($n, $used_count), "1"));
                     $set_combination = array_merge(array(0), $set_combination);
                     for ($ii = 0, $jj = 0, $not_used_index = 0; $ii < $n && $jj < count($set_combination); $ii++) {
                         if ($used[$ii] == -1) {
@@ -60,21 +53,16 @@ class Foo {
                             $not_used_index++;
                         }
                     }
-
-                    $used_count += $combination[$j];
+                    $used_count = bcadd($used_count, $combination[$j]);
                     unset($combination[$j]);
                     $combination = array_values($combination);
-
-                    $skipped = gmp_add($skipped, gmp_mul(gmp_sub(gmp_init($combination_number), gmp_init(1)), $x));
-
+                    $skipped = bcadd($skipped, bcmul(bcsub($combination_number, "1"), $x));
                     $set_index++;
                     break;
                 }
             }
-
         }
 //        echo join(', ', $used), "\r\n";
-
         $map = array();
         for ($i = 0; $i < count($used); $i++) {
             $map[$used[$i]] = array();
@@ -87,34 +75,29 @@ class Foo {
             array_push($sarr, join(', ', $map[$i]));
         }
         return '{'.join('}, {', $sarr).'}';
-
     }
-
     function getPartitionCountInBlock($combination) {
         $n = 0;
         for ($i = 0; $i < count($combination); $i++) {
             $n += $combination[$i];
         }
-
-        $count = gmp_init(1);
+        $count = "1";
         for ($j = 0; $j < count($combination); $j++) {
-            $count = gmp_mul($count, $this->getCombinations($combination[$j], $n));
+            $count = bcmul($count, $this->getCombinations($combination[$j], $n));
             $n -= $combination[$j];
         }
-
         $l = 1;
         for ($j = 1; $j < count($combination); $j++) {
             if ($combination[$j - 1] == $combination[$j]) {
                 $l++;
             } else {
-                $count = gmp_div($count, $this->getFactorial($l));
+                $count = bcdiv($count, $this->getFactorial($l));
                 $l = 1;
             }
         }
-        $count = gmp_div($count, $this->getFactorial($l));
+        $count = bcdiv($count, $this->getFactorial($l));
         return $count;
     }
-
     function getAllSumCombinations($n) {
         $combinations = array();
         for ($combinationSize = $n; $combinationSize > 0; $combinationSize--) {
@@ -124,7 +107,6 @@ class Foo {
             }
             array_push($currentCombination, $n - $combinationSize + 1);
             array_push($combinations, $currentCombination);
-
             for ($haveNext = true; $haveNext;) {
                 $haveNext = false;
                 $suffixSum = 0;
@@ -148,11 +130,9 @@ class Foo {
         }
         return $combinations;
     }
-
     function getCombinationByIndex($index, $k, $n) {
         $combination = array();
         $skipped = gmp_init(0);
-
         for ($i = 1; count($combination) < $k; $i++) {
             $c = $this->getCombinations($k - 1 - count($combination), $n - $i);
             if (gmp_cmp(gmp_add($skipped, $c), gmp_init($index)) >= 0) {
@@ -163,25 +143,20 @@ class Foo {
         }
         return $combination;
     }
-
     function getCombinations($k, $n) {
         $c = $this->getFactorial($n);
         $c = gmp_div($c, $this->getFactorial($k));
         $c = gmp_div($c, $this->getFactorial($n - $k));
         return $c;
     }
-
     function getFactorial($n) {
-        $fact = gmp_init(1);
+        $fact = "1";
         for ($i = 1; $i <= $n; $i++) {
-            $fact = gmp_mul($fact, $i);
+            $fact = bcmul($fact, (string) $i);
         }
         return $fact;
     }
-
 }
-
 $foo = new Foo;
-echo $foo->main(202, 6);
-
+echo $foo->main("203", "10");
 ?>
