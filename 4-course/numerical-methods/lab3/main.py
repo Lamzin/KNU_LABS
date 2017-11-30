948d433e2c7c1cdd5f3e23bbc31c7db219ff21d6
lama <olehtomas@gmail.com>
2017-12-01 01:51:08 +0200

    lab3: version 2

--- 4-course/numerical-methods/lab3/main.py
+++ 4-course/numerical-methods/lab3/main.py
@@ -17,8 +17,7 @@ u_kr = 4
 h = 1 / N
 tau = T / M
 alpha1, alpha2 = 1, 1
-mu2 = 0
-gamma1 = gamma * R / lamda
+gamma1 = gamma / lamda
 beta1, beta2 = 0, gamma1
 
 
@@ -27,12 +26,13 @@ def get_sigma(i):
     return sigma[i]
 
 
-def u_env(t):
+def u_env(t_level):
+    t = t_level * tau
     return sin(0.01 * t)
 
 
-def x1(x):
-    return x / R
+# def x1(x):
+#     return x / R
 
 
 def t1(t):
@@ -47,17 +47,17 @@ def v0():
     return (273 - u_kr) / u_kr
 
 
-x0_ = integrate((x1(x)) ** 2, (x, 0, h)) / h
+x0_ = integrate((x) ** 2, (x, 0, h)) / h
 
-xN_ = integrate((x1(x)) ** 2, (x, R, R - h)) / h
+xN_ = integrate((x) ** 2, (x, 1, 1 - h)) / h
 
 
 def xi_(i):
-    return integrate((x1(x)) ** 2, (x, (i - 1), (i + 1))) / h / 2
+    return integrate((x) ** 2, (x, (i - 1)/N, (i + 1)/N)) / h / 2
 
 
 def pi_(i):
-    return x1(h * (i - 0.5))
+    return h * (i - 0.5)
 
 
 def bi_(sigma, i):
@@ -72,20 +72,24 @@ def di_(sigma, i):
     return sigma * tau * pi_(i) / h / h
 
 
-def get_next_layer(y_prev):
+def get_next_layer(y_prev, level):
     sigma = get_sigma(1)
     matrix = [[0 for j in range(N + 1)] for i in range(N + 1)]
 
+    mu2 = (gamma / lamda) * u_env(level)
+
     b0 = sigma * tau * alpha1 * pi_(1) / h / h
     c0 = - alpha1 * x0_ / 2 - b0
-    phi0 = -tau * x0_ * mu2 / h \
-           - alpha1 * x0_ * y_prev[0] / 2 \
+
+    # phi0 = -tau * x0_ * mu2 / h \
+    phi0 = - alpha1 * x0_ * y_prev[0] / 2 \
            - (1 - sigma) * tau * alpha1 * pi_(1) * (y_prev[1] - y_prev[0]) / h / h
 
     dN = sigma * tau * alpha2 * pi_(N - 1) / h / h
     cN = - alpha1 * x0_ / 2 - dN
-    phiN = (1 - sigma) * tau * beta2 * x1(R - h) * y_prev[N - 1] / h \
-           - tau * xN_ * mu2 / h - alpha2 * xN_ * y_prev[N - 1] / 2 \
+    phiN = (1 - sigma) * tau * beta2 * (1 - h) * y_prev[N - 1] / h \
+           - tau * xN_ * mu2 / h \
+           - alpha2 * xN_ * y_prev[N - 1] / 2 \
            + (1 - sigma) * tau * alpha2 * pi_(N - 1) * (y_prev[N - 1] - y_prev[N - 2]) / h / h
 
     matrix[0][0] = c0
@@ -122,8 +126,8 @@ def get_next_layer(y_prev):
 
 def main():
     yi_cur = [1 for i in range(N + 1)]
-    for j in range(1,M):
-        yi_cur = get_next_layer(yi_cur)
+    for j in range(M):
+        yi_cur = get_next_layer(yi_cur, j)
         print(yi_cur)
 
 
